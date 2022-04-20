2788d2804e03ce24b52ed553721d5efd351047f5
rfinnegan <81394236+rfinnegan@users.noreply.github.com>
2022-04-20 15:13:31 -0400

    changes made to data and png clients

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,11 +1,12 @@
 import Clients from '../../../assets/Clients.png'
+import './Clients.css'
 
 const ClientsList = () => {
   return (
     <div>
       <h2>Clients</h2>
       <div>
-        <img src={Clients} height="55%" width="55%"/>
+        <img src={Clients} className="client-container"/>
       </div>
     </div>
   )
